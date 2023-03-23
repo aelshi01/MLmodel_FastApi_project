233246fc47b21b481362e6b64f5171d13db90037
Adam Elshimi <adamelshimi@Adams-Air.fritz.box>
2023-03-23 12:16:09 +0100

    testing github actions to allow for pytest to work

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 11:59:31 2023
+
+@author: adamelshimi
+"""
+
+def hello_world():
+
+    print("hello world")
+
+if __name__=="__main__":
+    hello_world() 
---
+++ test_main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 12:00:11 2023
+
+@author: adamelshimi
+"""
+from main import hello_world
+import pytest
+
+
+def test_hello_world():
+    assert hello_world == 'hello world'
