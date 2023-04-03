686e86d89abecab1944240c4ade59e9c30ec8d30
Adam Elshimi <adamelshimi@Adams-Air.fritz.box>
2023-04-03 14:46:02 +0200

    fix mode.py

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
-
+from ml.data import process_data
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
