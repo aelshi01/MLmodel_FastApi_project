1516ad4dbb43709829c74da71d8c8a71f95bdada
Adam Elshimi <adamelshimi@Adams-Air.fritz.box>
2023-04-06 15:05:19 +0200

    change requirements dir

--- starter/setup.py
+++ starter/setup.py
@@ -1,10 +1,8 @@
 import setuptools
-from setuptools import setup, find_packages
 
 setuptools.setup(
     name="starter",
     version="0.0.0",
     description="Starter code.",
     author="Adam Elshimi",
-    packages=find_packages()
 )
